a710c99a580fe8486c59df706505c8c8e119b6c4
Manh-Hai Bui <162355216+Manh-Hai-Bui@users.noreply.github.com>
2026-05-17 16:37:23 +0200

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ setTimeout(()=>{
   sdk.setSuperContent(`${JSON.stringify(newsletterJSON1, null, 2)}`)
   console.log('Setting supercontent')
 
-  sdk.setContent(`SET @JsonData = ${JSON.stringify(newsletterJSON1, null, 2)}`)
+  sdk.setContent(`%%[ SET @JsonData = ${JSON.stringify(newsletterJSON1, null, 2)} ]%%`)
   console.log('Setting setContent')
   
   }, 1000);
