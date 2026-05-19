4186da63ce3c45c1d12e3ea64e96e6e3026600a4
Manh-Hai Bui <162355216+Manh-Hai-Bui@users.noreply.github.com>
2026-05-19 20:12:14 +0200

    Update json.js

--- scripts/json.js
+++ scripts/json.js
@@ -151,9 +151,8 @@ const newsletterJSON1 = {
     ]
 }
 
-
 const newsletterJSON2 = {
-  "subject": {
+    "subject": {
         "value" : {
             "de" : "Willkommen in München!",
             "en" : "Welcome to Munich"
@@ -171,155 +170,143 @@ const newsletterJSON2 = {
             "en" : "MA000MUNICH"
         }
     },
-  "contentBlocks": [
-    {
-      "contentBlockId": "TeaserImage_Demo",
-      "contentBlockKey": "TeaserImage_Demo",
-      "type": "teaser",
-      "isVisible": true,
-      "content": {
-        "imageUrl": {
-          "ampScriptVar" : "teaserImageUrl",
-          "type": "input",
-          "label" : "Image URL",
-          "value": {
-                      "de" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/6933f20f-2ab6-4c7f-bfd6-d49c68088cbb.png",
-                      "en" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/6933f20f-2ab6-4c7f-bfd6-d49c68088cbb.png"
-                  },
+    "contentBlocks": [
+        {
+            "contentBlockId": "TeaserImage_Demo",
+            "contentBlockKey": "TeaserImage_Demo",
+            "type": "teaser",
+            "isVisible": true,
+            "content": [{
+              "type": "input",
+              "label" : "Image URL",
+              "ampScriptVar" : "teaserImageUrl",
+              "value": 
+                {
+                    "de" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/6933f20f-2ab6-4c7f-bfd6-d49c68088cbb.png",
+                    "en" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/6933f20f-2ab6-4c7f-bfd6-d49c68088cbb.png"
+                }
+            },{
+              "type": "input",
+              "label" : "Image alt text",
+              "ampScriptVar" : "imageAlt",
+              "value": {
+                  "de" : "Willkommen in München!",
+                  "en" : "Welcome to Munich"
+              } 
+            }],
+            "settings": {
+                "width": "600px",
+            }
         },
-        "imageAltText": {
-          "ampScriptVar" : "imageAlt",
-          "type": "input",
-          "label" : "Image alt text",
-          "value": {
-                      "de" : "Willkommen in München!",
-                      "en" : "Welcome to Munich!"
-                  },
-        }
-      },
-      "settings": {
-        "width": "600px",
-      }
-    },
-    {
-      "contentBlockId": "headline_h1_demo",
-      "contentBlockKey": "headline_h1_demo",
-      "type": "headline",
-      "isVisible": true,
-      "content": {
-        "text": {
-          "ampScriptVar" : "headline_h1",
-          "type": "input",
-          "label" : "H1 Text",
-          "value": {
-                      "de" : "Willkommen in München!",
-                      "en" : "Welcome to Munich!"
-                  },
+        {
+            "contentBlockId": "headline_h1_demo",
+            "contentBlockKey": "headline_h1_demo",
+            "type": "headline",
+            "isVisible": true,
+            "content": [{
+                "type": "input",
+                "label" : "H1 Text",
+                "ampScriptVar" : "headline_h1",
+                "value": {
+                      "de" : "Willkommen in München",
+                      "en" : "Welcome to Munich"
+                  }
+            }],
+            "settings": {
+                "fontFamily": "Arial, sans-serif",
+                "fontfontSize": "24px",
+                "fontWeight": "bold"
+            }
         },
-      },
-      "settings": {
-        "fontFamily": "Arial, sans-serif",
-        "fontfontSize": "24px",
-        "fontWeight": "bold"
-      }
-    },
-    {
-      "contentBlockId": "bodyText_Demo",
-      "contentBlockKey": "bodyText_Demo",
-      "type": "text",
-      "isVisible": true,
-      "content": {
-        "text": {
-          "ampScriptVar" : "bodyText",
-          "type": "input",
-          "label" : "Text",
-          "value": {
+        {
+            "contentBlockId": "bodyText_Demo",
+            "contentBlockKey": "bodyText_Demo",
+            "type": "text",
+            "isVisible": true,
+            "content": [{
+                "type": "input",
+                "label" : "Text",
+                "ampScriptVar" : "bodyText",
+                "value": {
                       "de" : "Das Stadtbild Münchens wird von jahrhundertealte Bauwerken und zahlreichen Museen geprägt. Die bayerische Landeshauptstadt ist für das alljährliche Oktoberfest und ihre Bierhallen bekannt, darunter vor allem das 1589 eröffnete Hofbräuhaus. In der Altstadt am zentralen Marienplatz stehen Wahrzeichen wie das neugotische Neue Rathaus, dessen beliebtes Glockenspiel Melodien und Geschichten aus dem 16. Jahrhundert spielt.",
                       "en" : "Munichs cityscape is characterized by centuries-old buildings and numerous museums. The Bavarian capital is known for the annual Oktoberfest and its beer halls, most notably the Hofbräuhaus, which opened in 1589. In the Old Town on the central Marienplatz, landmarks such as the neo-Gothic New Town Hall stand out, whose popular glockenspiel plays melodies and stories from the 16th century."
-                  }
-        },
-      },
-      "settings": {
-        "fontFamily": "Arial, sans-serif",
-        "fontfontSize": "14px",
-        "lineHeight": "140%"
-      }
-    },
-    {
-      "contentBlockId": "2col_mixed_demo",
-      "contentBlockKey": "2col_mixed_demo",
-      "type": "2colMixed",
-      "isVisible": true,
-      "content": {
-        "imageUrl1": {
-          "ampScriptVar" : "2colImageUrl",
-          "type": "input",
-          "label" : "Image 1",
-          "value": {
-                      "de" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/d8df5e95-55e6-4f09-980f-a0b7c004e514.png",
-                      "en" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/d8df5e95-55e6-4f09-980f-a0b7c004e514.png"
                   },
+            }],
+            "settings": {
+                "fontFamily": "Arial, sans-serif",
+                "fontfontSize": "14px",
+                "lineHeight": "140%"
+            }
         },
-        "headline1": {
-          "ampScriptVar" : "2colHeadline",
-          "type": "input",
-          "label" : "Headline",
-          "value": {
-                      "de" : "Innenstadt",
-                      "en" : "Citycenter"
-                  }
-        },
-        "text1": {
-          "ampScriptVar" : "2colText",
-          "type": "input",
-          "label" : "Text",
-          "value": {
-                      "de" : "München ist Sitz des Bayerischen Landtages, der Bayerischen Staatsregierung, Verwaltungssitz des die Stadt umgebenden Landkreises München mit dessen Landratsamt sowie des bayerischen Bezirks Oberbayern und des Regierungsbezirks Oberbayern.",
-                      "en" : "Munich is the seat of the Bavarian State Parliament and the Bavarian State Government, as well as the administrative center of the Munich district surrounding the city—including its district office—and of the Bavarian district of Upper Bavaria and the administrative region of Upper Bavaria."
-                  },
-        }
-      },
-      "settings": {
-        "direction" : "ltr"
-      }
-    },
-    {
-      "contentBlockId": "2col_mixed_demo",
-      "contentBlockKey": "2col_mixed_demo",
-      "type": "2colMixed",
-      "isVisible": true,
-      "content": {
-        "imageUrl1": {
-          "ampScriptVar" : "2colImageUrl",
-          "type": "input",
-          "label" : "Image 1",
-          "value": {
+        {
+            "contentBlockId": "2col_mixed_demo",
+            "contentBlockKey": "2col_mixed_demo",
+            "type": "2colMixed",
+            "isVisible": true,
+            "content": [{
+                "type": "input",
+                "label" : "Image URL",
+                "ampScriptVar" : "2colImageUrl",
+                "value": {
                       "de" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/7fbcde2b-0b1e-4c8f-8924-a2ed4ad2bc97.png",
                       "en" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/7fbcde2b-0b1e-4c8f-8924-a2ed4ad2bc97.png"
                   }
-        },
-        "headline1": {
-          "ampScriptVar" : "2colHeadline",
-          "type": "input",
-          "label" : "Headline",
-          "value": {
+            },{
+                "type": "input",
+                "label" : "Headline",
+                "ampScriptVar" : "2colHeadline",
+                "value": {
                       "de" : "Stadttor",
                       "en" : "City Gate"
                   },
-        },
-        "text1": {
-          "ampScriptVar" : "2colText",
-          "type": "input",
-          "label" : "Text",
-          "value": {
+            },{
+                "type": "input",
+                "label" : "Body Text",
+                "ampScriptVar" : "2colText",
+                "value": {
                       "de" : "Die „porta Sentlingeriorum“ diente in der zweiten Stadtbefestigung Münchens, die ab dem Ende des 13. Jahrhunderts errichtet wurde, als südliches Haupttor. Die Auswahl der Abbildungen stammt aus den rund 25.000 München-Fotos der Photothek des Zentralinstituts für Kunstgeschichte.",
                       "en" : "The “porta Sentlingeriorum” served as the main southern gate in Munich’s second city fortifications, which were constructed beginning in the late 13th century. The images were selected from the approximately 25,000 photos of Munich held in the photo library of the Central Institute for Art History."
+                  }
+            }
+          ],
+            "settings": {
+              "direction" : "ltr"
+            }
+        },
+        {
+            "contentBlockId": "2col_mixed_demo",
+            "contentBlockKey": "2col_mixed_demo",
+            "type": "2colMixed",
+            "isVisible": true,
+            "content": [{
+                "type": "input",
+                "label" : "Image URL",
+                "ampScriptVar" : "2colImageUrl",
+                "value": {
+                      "de" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/7fbcde2b-0b1e-4c8f-8924-a2ed4ad2bc97.png",
+                      "en" : "https://image.s51.sfmc-content.com/lib/fe2c11737364047c7c1275/m/1/7fbcde2b-0b1e-4c8f-8924-a2ed4ad2bc97.png"
+                  }
+            },{
+                "type": "input",
+                "label" : "Headline",
+                "ampScriptVar" : "2colHeadline",
+                "value": {
+                      "de" : "Stadttor",
+                      "en" : "City Gate"
                   },
+            },{
+                "type": "input",
+                "label" : "Body Text",
+                "ampScriptVar" : "2colText",
+                "value": {
+                      "de" : "Die „porta Sentlingeriorum“ diente in der zweiten Stadtbefestigung Münchens, die ab dem Ende des 13. Jahrhunderts errichtet wurde, als südliches Haupttor. Die Auswahl der Abbildungen stammt aus den rund 25.000 München-Fotos der Photothek des Zentralinstituts für Kunstgeschichte.",
+                      "en" : "The “porta Sentlingeriorum” served as the main southern gate in Munich’s second city fortifications, which were constructed beginning in the late 13th century. The images were selected from the approximately 25,000 photos of Munich held in the photo library of the Central Institute for Art History."
+                  }
+            }
+          ],
+            "settings": {
+              "direction" : "rtl"
+            }
         }
-      },
-      "settings": {
-        "direction" : "rtl"
-      }
-    }
-  ]
+    ]
 }
