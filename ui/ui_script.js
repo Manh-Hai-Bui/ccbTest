d92eee0162a432882ce514f37092555d7c8b16b3
Manh-Hai Bui <162355216+Manh-Hai-Bui@users.noreply.github.com>
2026-05-26 08:13:36 +0200

    Update

---
+++ ui/ui_script.js
@@ -0,0 +1,28 @@
+let sdk = new window.sfdc.BlockSDK({
+tabs: [/* */]
+,blockEditorWidth: 425
+});
+
+console.log('Testing block')
+
+/*setTimeout(()=>{
+  
+  sdk.setSuperContent(`${JSON.stringify(newsletterJSON1, null, 2)}`)
+  console.log('Setting supercontent')
+
+  sdk.setContent(`%%[ SET @JsonData = '${JSON.stringify(newsletterJSON1, null, 2)}' ]%%`)
+  console.log('Setting setContent')
+  
+  }, 1000);*/
+
+document.querySelector('#sendButton').addEventListener('click', (e) => {
+    const inputValue = document.querySelector('#contentIdInput').value;
+    let json = newsletterJSON1;
+
+    if(!inputValue || inputValue != 0){
+        json = newsletterJSON2;
+    } 
+
+    //sdk.setSuperContent(`${JSON.stringify(json, null, 2)}`)
+    //sdk.setContent(`%%[ SET @JsonData = '${JSON.stringify(json, null, 2)}' ]%%`)
+})
